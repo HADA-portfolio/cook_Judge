77a32cf56edc2cc9f12b6ab6f8472625d13312ff
HIRO-HANE <hiroto.hada@outlook.jp>
2025-05-26 23:06:22 +0900

    Update cook_flask.py

--- cook_flask.py
+++ cook_flask.py
@@ -36,13 +36,13 @@ HTML_HEADER = f"""
 /*スマホの画面（600px以下）のときに文字やボタンを大きくする*/
 @media screen and (max-width: 600px) {{
     body {{
-        font-size: 1.1em;
+        font-size: 1em;
     }}
     input, button, .button {{
-        font-size: 1.3em;
+        font-size: 1em;
     }}
     h1, h2 {{
-        font-size: 1.1em;
+        font-size: 1em;
     }}
 }}
 </style>
